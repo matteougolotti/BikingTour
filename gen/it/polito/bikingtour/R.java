0217e6e48547f721aaa1144c7087024d35bd9d70
Jessica Lins Laureno <jessicalins81@gmail.com>
2014-06-25 17:19:12 +0200

    Adding drawer menu

--- gen/it/polito/bikingtour/R.java
+++ gen/it/polito/bikingtour/R.java
@@ -8,7 +8,24 @@
 package it.polito.bikingtour;
 
 public final class R {
+    public static final class array {
+        /**  Nav Drawer List Item Icons 
+ Keep them in order as the titles are in 
+         */
+        public static final int nav_drawer_icons=0x7f070001;
+        /**  Nav Drawer Menu Items 
+         */
+        public static final int nav_drawer_items=0x7f070000;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int counter_text_bg=0x7f040004;
+        public static final int counter_text_color=0x7f040005;
+        public static final int list_background=0x7f040001;
+        public static final int list_background_pressed=0x7f040002;
+        public static final int list_divider=0x7f040003;
+        public static final int list_item_title=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -18,29 +35,51 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int counter_bg=0x7f020000;
+        public static final int ic_drawer=0x7f020001;
+        public static final int ic_help=0x7f020002;
+        public static final int ic_home=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_review=0x7f020005;
+        public static final int ic_search=0x7f020006;
+        public static final int list_item_bg_normal=0x7f020007;
+        public static final int list_item_bg_pressed=0x7f020008;
+        public static final int list_selector=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int container=0x7f080000;
+        public static final int action_settings=0x7f0a0007;
+        public static final int counter=0x7f0a0005;
+        public static final int drawer_layout=0x7f0a0000;
+        public static final int frame_container=0x7f0a0001;
+        public static final int icon=0x7f0a0003;
+        public static final int list_slidermenu=0x7f0a0002;
+        public static final int title=0x7f0a0004;
+        public static final int txtLabel=0x7f0a0006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int drawer_list_item=0x7f030001;
+        public static final int fragment_help=0x7f030002;
+        public static final int fragment_home=0x7f030003;
+        public static final int fragment_review=0x7f030004;
+        public static final int fragment_search=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int add_route=0x7f050003;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int my_routes=0x7f050004;
+        public static final int action_settings=0x7f060002;
+        public static final int add_route=0x7f060003;
+        public static final int app_name=0x7f060000;
+        /**  Content Description 
+         */
+        public static final int desc_list_item_icon=0x7f060005;
+        public static final int hello_world=0x7f060001;
+        public static final int my_routes=0x7f060004;
     }
     public static final class style {
         /** 
@@ -64,10 +103,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
     }
 }
