c044251707e06f36cd7606152ece136e72874965
matteo.ugolotti <matteo.ugolotti@gmail.com>
2014-07-22 00:54:19 +0200

    Worked on TourFragment

--- src/it/polito/bikingtour/TourFragment.java
+++ src/it/polito/bikingtour/TourFragment.java
@@ -1,20 +1,26 @@
 package it.polito.bikingtour;
 
-import it.polito.model.Route;
-import it.polito.model.RoutesContainer;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import it.polito.bikingtour.R;
+import it.polito.adapter.TourMediaAdapter;
 import it.polito.model.Tour;
 import it.polito.model.ToursContainer;
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.TabHost.TabSpec;
@@ -29,7 +35,7 @@ public class TourFragment extends Fragment{
 	public void onCreate(Bundle savedInstanceState) { 
 	    super.onCreate(savedInstanceState);   
 	    this.toursContainer = ToursContainer.newInstance(getActivity());
-	    //TODO get tour id from fragment arguments
+	    this.tour = toursContainer.getTour(this.getArguments().getInt("tourId"));
 	}
 	
 	@Override
@@ -59,8 +65,86 @@ public class TourFragment extends Fragment{
         tabHost.addTab(specVideos);
         tabHost.addTab(specInfo);
         
-        //TODO complete fragment view setup
+        ListView picturesListView = (ListView) rootView.findViewById(R.id.tour_pictures_listview);
+        ArrayList<Bitmap> pictures = new ArrayList<Bitmap>();
+        for(String picture : tour.getPictures()){
+        	Bitmap newPicture = getBitmapByName(picture);
+        	if(newPicture != null)
+        		pictures.add(newPicture);
+        }
+        if(pictures.size() > 0){
+        	picturesListView.setAdapter(new TourMediaAdapter(getActivity(), pictures));
+        }else{
+        	TextView picturesText = new TextView(getActivity());
+        	picturesText.setText(getActivity().getString(R.string.no_pictures));
+        	LinearLayout tourTabPictures = (LinearLayout) rootView.findViewById(R.id.tour_tab_pictures);
+        	tourTabPictures.addView(picturesText);
+        }
+        
+        ListView videosListView = (ListView) rootView.findViewById(R.id.tour_videos_listview);
+        ArrayList<Bitmap> videos = new ArrayList<Bitmap>();
+        for(String video : tour.getVideos()){
+        	Bitmap newVideo = getVideoPreviewByName(video);
+        	if(newVideo != null)
+        		videos.add(newVideo);
+        }
+        if(videos.size() > 0){
+        	videosListView.setAdapter(new TourMediaAdapter(getActivity(), videos));
+        }else{
+        	TextView videosText = new TextView(getActivity());
+        	videosText.setText(getActivity().getString(R.string.no_videos));
+        	LinearLayout tourTabVideos = (LinearLayout) rootView.findViewById(R.id.tour_tab_videos);
+        	tourTabVideos.addView(videosText);
+        }
         
         return rootView;
     }
+	
+	private Bitmap getBitmapByName(String fileName){
+        InputStream is = null;
+        Bitmap bitmap = null;
+        int scaled_size = getScaledSize();
+        try{
+        	is = getActivity().openFileInput(fileName);
+        	bitmap = BitmapFactory.decodeStream(is);
+            bitmap = Bitmap.createScaledBitmap(bitmap,  (int)(scaled_size), (int)(scaled_size), false);
+        }catch(IOException e){
+            Log.d("TourFragment.getBitmapByName", e.getMessage());
+        }
+        
+        return bitmap;
+    }
+
+    private int getScaledSize(){
+    	DisplayMetrics metrics = new DisplayMetrics();
+	    getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        if(metrics.densityDpi == DisplayMetrics.DENSITY_LOW)
+            return (int)96;
+        else if(metrics.densityDpi == DisplayMetrics.DENSITY_MEDIUM)
+            return (int)192;
+        else if(metrics.densityDpi == DisplayMetrics.DENSITY_HIGH)
+            return (int)288;
+        else if(metrics.densityDpi == DisplayMetrics.DENSITY_XHIGH)
+            return (int)384;
+        else if(metrics.densityDpi == DisplayMetrics.DENSITY_XXHIGH)
+            return (int)576;
+        return 0;
+    }
+	
+    private Bitmap getVideoPreviewByName(String fileName){
+    	Bitmap videoPreview = null;
+    	try{
+    		MediaMetadataRetriever mRetriever = new MediaMetadataRetriever();
+    		File file = new File(fileName);
+    		if(file.exists()){
+    			mRetriever.setDataSource(fileName);
+    			videoPreview = mRetriever.getFrameAtTime((long)1000);
+    		}
+    	}catch(Exception e){
+    		Log.d("TourFragment.getVideoPreviewByName", e.getMessage());
+    	}
+    	
+    	return videoPreview;
+    }
+    
 }
--- src/it/polito/bikingtour/TourReviewFragment.java
+++ src/it/polito/bikingtour/TourReviewFragment.java
@@ -3,10 +3,13 @@ package it.polito.bikingtour;
 import it.polito.adapter.TourArrayAdapter;
 import it.polito.model.ToursContainer;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 public class TourReviewFragment extends Fragment {
@@ -24,6 +27,20 @@ public class TourReviewFragment extends Fragment {
         tours = ToursContainer.newInstance(getActivity());
         ListView myTours = (ListView) rootView.findViewById(R.id.myToursListView);
         myTours.setAdapter(new TourArrayAdapter(getActivity(), tours.getTours()));
+        myTours.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				Bundle bundle = new Bundle();
+				bundle.putLong("tourId", position);
+				Fragment newFragment = new TourFragment();
+				newFragment.setArguments(bundle);
+				FragmentTransaction transaction = getFragmentManager().beginTransaction();
+				transaction.replace(R.id.frame_container, newFragment);
+				transaction.addToBackStack(null);
+				transaction.commit();
+			}
+        });
         
         return rootView;
     }
